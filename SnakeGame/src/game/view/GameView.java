bf90d0ceb7b3a5fbc2244b134efec3efad3d3c26
Rene Nespithal <rene.nespithal@campus.lmu.de>
2015-10-28 19:39:55 +0100

    added Image for Snake

--- SnakeGame/src/game/view/GameView.java
+++ SnakeGame/src/game/view/GameView.java
@@ -23,27 +23,27 @@ public class GameView extends Pane{
 	private Pane highscorePane;
 	public GameView(GameModel model) {
 
+		Image imageSnake = new Image("file:src/images/snakePart.jpg", 20, 20, true, true);
 
-		Rectangle snakeHead = new Rectangle(20, 20);
-		snakeHead.xProperty().bind(new IntegerBinding() {
+		ImageView ivSnake = new ImageView();
+		ivSnake.setImage(imageSnake);
+
+
+		ivSnake.xProperty().bind(new IntegerBinding() {
 			{bind(model.getSnake().getXProperty());}
-			
-			@Override
-			protected int computeValue() {
-				return model.getSnake().getX() *20;
-			}
-			});
-		
-		snakeHead.yProperty().bind(new IntegerBinding() {
-			{bind(model.getSnake().getYProperty());}
-		
-			@Override
-			protected int computeValue() {
 
-				return model.getSnake().getY() *20;
-			}
-		});
-		snakeHead.setFill(Color.GREEN);
+			@Override
+			protected int computeValue() {
+				return model.getSnake().getX()*20;
+			}});
+
+		ivSnake.yProperty().bind(new IntegerBinding() {
+			{bind(model.getSnake().getYProperty());}
+
+			@Override
+			protected int computeValue() {
+				return model.getSnake().getY()*20;
+			}});
 
 		Image imageYin = new Image("file:src/images/Yin.png", 20, 20, true, true);
 
@@ -60,12 +60,15 @@ public class GameView extends Pane{
 			}});
 
 		ivYin.yProperty().bind(new IntegerBinding() {
-			{bind(model.getYinYang().getYProperty());}
+			{
+				bind(model.getYinYang().getYProperty());
+			}
 
 			@Override
 			protected int computeValue() {
-				return model.getYinYang().getY()*20;
-			}});
+				return model.getYinYang().getY() * 20;
+			}
+		});
 
 		Image imageFood = new Image("file:src/images/Apple.jpg", 20, 20, true, true);
 
@@ -81,39 +84,37 @@ public class GameView extends Pane{
 			}});
 
 		ivFood.yProperty().bind(new IntegerBinding() {
-			{bind(model.getFood().getYProperty());}
+			{
+				bind(model.getFood().getYProperty());
+			}
 
 			@Override
 			protected int computeValue() {
-				return model.getFood().getY()*20;
-			}});
+				return model.getFood().getY() * 20;
+			}
+		});
 
 
-
-				
-		HBox hBox = new HBox(); 
+		HBox hBox = new HBox();
 		Label highscore = new Label();
 		highscore.textProperty().bind(new StringBinding() {
-			{ bind(model.getHighscore().getValueProperty()); }
-			
+			{
+				bind(model.getHighscore().getValueProperty());
+			}
+
 			@Override
 			protected String computeValue() {
-				return "Highscore: "+model.getHighscore().getValue();
+				return "Highscore: " + model.getHighscore().getValue();
 			}
 		});
 		highscore.setFont(new Font(40));
 
 		hBox.setAlignment(Pos.CENTER);
 		hBox.getChildren().add(highscore);
-		
-	
-		Pane snakePane = new Pane();
-		snakePane.getChildren().addAll(snakeHead);
-		
 
 
 		Pane imagePane = new Pane();
-		imagePane.getChildren().addAll(ivYin, ivFood);
+		imagePane.getChildren().addAll(ivYin, ivFood, ivSnake);
 
 		highscorePane = new StackPane(); 
 		highscorePane.getChildren().add(hBox);
@@ -121,7 +122,7 @@ public class GameView extends Pane{
 		highscorePane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null)));
 		highscorePane.autosize();
 		
-		this.getChildren().addAll(snakePane,highscorePane,imagePane);
+		this.getChildren().addAll(highscorePane,imagePane);
 	}
 
 
